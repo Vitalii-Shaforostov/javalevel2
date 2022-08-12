5a18799b4f93a9c5e9ca2820cb08b4ca8506ae7f
Vitalii <vs_rusik@mail.ru>
2022-08-12 21:02:29 +0300

    All rebuilded

--- src/main/java/ru/levelup/j2/Main.java
+++ src/main/java/ru/levelup/j2/Main.java
@@ -1,9 +1,12 @@
 package ru.levelup.j2;
 
 
-
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 public class Main {
+
+    private static Gson gson = new GsonBuilder().create();
 
     public static int sum(int a, int b){
         return a+b;
@@ -12,6 +15,16 @@ public class Main {
 
     public static int sub(int a, int b){
         return a-b;
+    }
+
+    // Serializatoin
+    public static String userToJson(User user){
+        return gson.toJson(user);
+    }
+
+    // Deserialization
+    public static User jsonToUser(String message){
+        return gson.fromJson(message, User.class);
     }
 }
 
--- src/test/java/ru/levelup/j2/MainTest.java
+++ src/test/java/ru/levelup/j2/MainTest.java
@@ -2,6 +2,9 @@ package ru.levelup.j2;
 
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.List;
+
 import static org.junit.Assert.*;
 
 public class MainTest {
@@ -10,6 +13,7 @@ public class MainTest {
 
     @Test
     public void sum1() {
+
         if (Main.sum( 1, 2 ) != 3){
             fail("Сумма 1 и 2 должна быть 3");
         }
@@ -17,9 +21,37 @@ public class MainTest {
 
     @Test
     public void sub1() {
-        if (Main.sub( 5, 3 ) != 2){
+
+        assertEquals(2, Main.sub( 5, 3 ) );
+  /*      if (Main.sub( 5, 3 ) != 2){
             fail("Разность 5 и 3 должна быть 2");
         }
+*/
+
+    }
+
+    @Test
+    public void userToJson() {
+        User user = new User(123, "user1", Arrays.asList("admin", "moderator"));
+        String json = Main.userToJson(user);
+        String expected = "{\"id\":123,\"login\":\"user1\",\"roles\":[\"admin\",\"moderator\"]}";
+
+    /*   if(!json.equals(expected)) {
+            fail("Json format is not valid: expected = " + expected + ", actual = " + json);
+        }
+*/
+
+      assertEquals(expected, json);
+
+    }
+
+    @Test
+    public void jsonToUser() {
+        String json = "{\"id\":113,\"login\":\"user2\",\"roles\":[\"admin\",\"creator\"]}";
+        User user = Main.jsonToUser(json);
+
+        User expected = new User (113, "user2", List.of("admin", "creator"));
+        assertEquals(expected, user);
 
     }
 }
